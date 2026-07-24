cfa7035c6f1a2a10a145a2ed9a8ec52ff6a1628e
ranjith-ui1 <ravuriranjithkumar2004@gmail.com>
2026-07-24 11:05:47 +0530

    Fix Login page import

--- frontend/src/pages/public/Register.jsx
+++ frontend/src/pages/public/Register.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import API from "../../api/axios";
+import "./css/Register.css";
 
 function Register() {
   const [name, setName] = useState("");
